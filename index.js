6fdb92ba6f403b455c6866cd2d0a9c0f56701dd0
Walter Garcia <walteregv@gmail.com>
2020-08-05 16:12:11 -0600

    conexión a la base de datos

--- index.js
+++ index.js
@@ -19,6 +19,39 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.listen(3000,() => console.log("Server start on port 3000"))
 
 /**
+ * Conexión con la base de datos
+ *
+ */
+const {MongoClient} = require('mongodb');
+const { listDatabases } = require("./listDatabases")
+
+async function main(){
+    
+    const uri = "mongodb+srv://adminTaller:adminTaller123@cluster0.a0nse.mongodb.net/Testing?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+
+
+
+/**
  * Rutas
  */
 
