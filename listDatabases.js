e943dd0ec54cf95e8304bb3d841f4e338db5a4a8
Walter Garcia <walteregv@gmail.com>
2020-08-05 15:37:21 -0600

    Configuracion de la conexion de a la base de datos

---
+++ listDatabases.js
@@ -0,0 +1,8 @@
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+exports.listDatabases = listDatabases;
