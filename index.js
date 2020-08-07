81240503317db6ebb61a29791b32f2b931169686
Walter Garcia <walteregv@gmail.com>
2020-08-07 11:37:29 -0600

    conexion con la base de datos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
 
@@ -14,3 +15,19 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use("/",user)
 
 app.listen(PORT,() => console.log("Servidor funcionando en puerto",PORT))
+
+const uri = 'mongodb+srv://adminTaller:adminTaller123@cluster0.a0nse.mongodb.net/Taller?retryWrites=true&w=majority';
+
+const dbConnetion = async () => {
+
+    try {
+        
+       
+ await mongoose.connect(uir, {useNewUrlParser: true, useUnifiedTopology: true});
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+dbConnetion (); 
